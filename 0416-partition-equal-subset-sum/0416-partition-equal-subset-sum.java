40e46ae7a28483905f96ec04294db0297ace868a
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2025-03-22 12:15:59 +0530

    Time: 31 ms (69.6%), Space: 42.2 MB (87.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -8,20 +8,15 @@ class Solution {
             return false;
         }
         sum = sum / 2;
-        boolean dp[][] = new boolean[n + 1][sum + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= sum; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = false;
-                } else if (nums[i-1] == j) {
-                    dp[i][j] = true;
-                } else {
-                    boolean v1 = dp[i-1][j];
-                    boolean v2 = j > nums[i-1] ? dp[i-1][j-nums[i-1]] : false;
-                    dp[i][j] = v1 || v2;
+        boolean dp[] = new boolean[sum + 1];
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = sum; j >= 0; j--) {
+                if (j >= nums[i]) {
+                    dp[j] = dp[j] || dp[j - nums[i]];
                 }
             }
         }
-        return dp[n][sum];
+        return dp[sum];
     }
 }
