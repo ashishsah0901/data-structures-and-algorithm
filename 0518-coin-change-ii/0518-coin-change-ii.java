6ce36281b10ff4ec0505134a5113c7c828bb46c0
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2025-03-23 12:10:23 +0530

    Time: 4 ms (92.34%), Space: 41.5 MB (74.52%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -10,22 +10,4 @@ class Solution {
         }
         return dp[amount];
     }
-    private int count(int amount, int[] coins, int idx, int dp[][]) {
-        if (amount < 0) {
-            return 0;
-        }
-        if (amount == 0) {
-            return 1;
-        }
-        if (idx >= coins.length) {
-            return 0;
-        }
-        if (dp[amount][idx] != -1) {
-            return dp[amount][idx];
-        }
-        int sum = 0;
-        sum += count(amount, coins, idx + 1, dp);
-        sum += count(amount - coins[idx], coins, idx, dp);
-        return dp[amount][idx] = sum;
-    }
 }
