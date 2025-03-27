3faa708f746223c7b0328eccbc9d515123595606
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2025-03-27 16:37:01 +0530

    Time: 15 ms (94.81%), Space: 47 MB (30.62%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,43 +1,20 @@
 class Solution {
-    private boolean isPal(String s, int i, int j){
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-    private int cuts(String s, int i, int n, int dp[]) {
-        if (i == n) {
-            return 0;
-        }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        int min = Integer.MAX_VALUE;
-        for (int k = i; k < n; k++) {
-            if (isPal(s, i, k)) {
-                int curr = cuts(s, k + 1, n, dp) + 1;
-                min = Math.min(min, curr);
-            }
-        }
-        return dp[i] = min;
-    }
     public int minCut(String s) {
-        int n = s.length();
-        int dp[] = new int[n + 1];
-        for (int i = n - 1; i >= 0; i--) {
-            int min = Integer.MAX_VALUE;
-            for (int k = i; k < n; k++) {
-                if (isPal(s, i, k)) {
-                    int curr = dp[k + 1] + 1;
-                    min = Math.min(min, curr);
+        char[] c = s.toCharArray();
+        int n = c.length;
+        int[] cut = new int[n];
+        boolean[][] pal = new boolean[n][n];
+        
+        for(int i = 0; i < n; i++) {
+            int min = i;
+            for(int j = 0; j <= i; j++) {
+                if(c[j] == c[i] && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+                    pal[j][i] = true;  
+                    min = j == 0 ? 0 : Math.min(min, cut[j - 1] + 1);
                 }
             }
-            dp[i] = min;
+            cut[i] = min;
         }
-        return dp[0] - 1;
+        return cut[n - 1];
     }
 }
