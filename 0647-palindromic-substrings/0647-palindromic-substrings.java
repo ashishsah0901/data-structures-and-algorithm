e010c40c55aa4924caa00542c3a04d5318f0802c
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2025-04-02 16:58:38 +0530

    Time: 12 ms (45.05%), Space: 45.2 MB (32.54%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,23 +1,28 @@
 class Solution {
-    int count = 0;
-
-    public int countSubstrings(String s) {
-        if (s == null || s.length() == 0)
-            return 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            extendPalindrome(s, i, i);
-            extendPalindrome(s, i, i + 1);
-        }
-
-        return count;
-    }
-
-    private void extendPalindrome(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+    public int countSubstrings(String str) {
+        int n = str.length();
+        int count = 0;
+        boolean table[][] = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            table[i][i] = true;
             count++;
-            left--;
-            right++;
         }
+        for (int i = 0; i < n - 1; ++i) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                table[i][i + 1] = true;
+                count++;
+            }
+        }
+        for (int k = 3; k <= n; ++k) {
+            for (int i = 0; i < n - k + 1; ++i) {
+                int j = i + k - 1;
+                if (table[i + 1][j - 1]
+                    && str.charAt(i) == str.charAt(j)) {
+                    table[i][j] = true;
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
