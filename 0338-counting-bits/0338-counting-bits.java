758a1e0f087101192efcc636f45bd0da6ba8b023
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2025-04-02 15:47:48 +0530

    Time: 3 ms (34.76%), Space: 46.9 MB (32.76%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,20 +1,16 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        if(n <= 0) return dp;
-        dp[1] = 1;
-        if(n <= 1) return dp;
-        for(int i=2;i<=n;i++) {
-            dp[i] = recurse(i,n,dp);
+        int[] answer = new int[n + 1];
+        int mask = 1;
+        for (int i = 0; i <= n; i++) {
+            int num = i;
+            int sum = 0;
+            for (int j = 0; (j < 32 && num != 0); j++) {
+                sum += num & mask;
+                num >>= 1;
+            }
+            answer[i] = sum;
         }
-        return dp;
-    }
-    int recurse(int i, int n, int[] dp) {
-        if(i>n) return -1;
-        if(dp[i]!=0) return dp[i];
-        int ans = recurse(i>>1,n,dp) + (((i&1)==0) ? 0 : 1);
-        dp[i] = ans;
-        return ans;
+        return answer;
     }
 }
