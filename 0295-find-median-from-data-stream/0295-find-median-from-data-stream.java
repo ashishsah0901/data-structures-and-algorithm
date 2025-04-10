2ed881f814e16204bb4f0e21736b8d6560420c2d
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2025-04-10 23:42:46 +0530

    Time: 91 ms (82.63%), Space: 63.9 MB (43.4%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,39 +1,37 @@
 class MedianFinder {
-
-    PriorityQueue<Integer> min;
-    PriorityQueue<Integer> max;
+    PriorityQueue<Integer> minHeap, maxHeap;
 
     public MedianFinder() {
-        min = new PriorityQueue<>();
-        max = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<Integer>();
+        maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
     }
 
     public void addNum(int num) {
-        if (min.size() == 0 && max.size() == 0)
-            max.add(num);
-        else if (max.size() > min.size()) {
-            if (max.peek() > num) {
-                min.offer(max.poll());
-                max.offer(num);
-            } else {
-                min.offer(num);
-            }
-        } else {
-            if (num <= max.peek()) {
-                max.offer(num);
-            } else {
-                min.offer(num);
-                max.offer(min.poll());
-            }
-        }
+        // Add in corresponding heap
+        if (maxHeap.isEmpty() || num <= maxHeap.peek())
+            maxHeap.offer(num);
+        else
+            minHeap.offer(num);
+        // Check balance
+        if (maxHeap.size() > minHeap.size() + 1)
+            minHeap.offer(maxHeap.poll());
+        else if (minHeap.size() > maxHeap.size() + 1)
+            maxHeap.offer(minHeap.poll());
     }
 
     public double findMedian() {
-        if (max.size() > min.size()) {
-            return max.peek();
-        } else {
-            return (max.peek() + min.peek()) / 2.0;
+        if (maxHeap.size() == minHeap.size()) {
+            if (maxHeap.isEmpty())
+                return 0.0;
+            // Even length, take average of both peeks
+            double leftMax = maxHeap.peek(), rightMin = minHeap.peek();
+            return (leftMax + rightMin) / 2.0;
         }
+        // Odd length
+        if (maxHeap.size() > minHeap.size())
+            return (double) maxHeap.peek();
+        return (double) minHeap.peek();
+
     }
 }
 
