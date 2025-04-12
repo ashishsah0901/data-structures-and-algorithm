3be9e81945545d5c8b48817269e4d5991596a837
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2025-04-12 08:10:13 +0530

    Time: 398 ms (40%), Space: 48 MB (32%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.java
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.java
@@ -1,58 +1,41 @@
 class Solution {
-    long res = 0;
-    Set<String> visited = new HashSet<>();
 
-    long vectorToNumber(int[] digits) {
-        long num = 0;
-        for (int d : digits) num = num * 10 + d;
-        return num;
-    }
-
-    long fact(int n) {
-        long res = 1;
-        for (int i = 2; i <= n; i++) res *= i;
-        return res;
-    }
-
-    long totalPermutations(Map<Integer, Integer> freq, int total) {
-        long res = fact(total);
-        for (int count : freq.values()) res /= fact(count);
-        return res;
-    }
-
-    long permsWithZero(Map<Integer, Integer> freq, int total) {
-        if (!freq.containsKey(0) || freq.get(0) == 0) return 0;
-        freq.put(0, freq.get(0) - 1);
-        long res = fact(total - 1);
-        for (int count : freq.values()) res /= fact(count);
-        return res;
-    }
-
-    void genPal(int[] palin, int left, int right, int divisor, int total) {
-        if (left > right) {
-            long val = vectorToNumber(palin);
-            if (val % divisor == 0) {
-                Map<Integer, Integer> freq = new HashMap<>();
-                for (int d : palin) freq.put(d, freq.getOrDefault(d, 0) + 1);
-                String key = freq.toString();
-                if (!visited.contains(key)) {
-                    res += totalPermutations(freq, total) - permsWithZero(new HashMap<>(freq), total);
-                    visited.add(key);
-                }
+    public long countGoodIntegers(int n, int k) {
+        Set<String> dict = new HashSet<>();
+        int base = (int) Math.pow(10, (n - 1) / 2);
+        int skip = n & 1;
+        /* Enumerate the number of palindrome numbers of n digits */
+        for (int i = base; i < base * 10; i++) {
+            String s = Integer.toString(i);
+            s += new StringBuilder(s).reverse().substring(skip);
+            long palindromicInteger = Long.parseLong(s);
+            /* If the current palindrome number is a k-palindromic integer */
+            if (palindromicInteger % k == 0) {
+                char[] chars = s.toCharArray();
+                Arrays.sort(chars);
+                dict.add(new String(chars));
             }
-            return;
         }
 
-        for (int d = (left == 0 ? 1 : 0); d <= 9; d++) {
-            palin[left] = palin[right] = d;
-            genPal(palin, left + 1, right - 1, divisor, total);
+        long[] factorial = new long[n + 1];
+        factorial[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = factorial[i - 1] * i;
         }
-    }
+        long ans = 0;
+        for (String s : dict) {
+            int[] cnt = new int[10];
+            for (char c : s.toCharArray()) {
+                cnt[c - '0']++;
+            }
+            /* Calculate permutations and combinations */
+            long tot = (n - cnt[0]) * factorial[n - 1];
+            for (int x : cnt) {
+                tot /= factorial[x];
+            }
+            ans += tot;
+        }
 
-    public long countGoodIntegers(int total, int divisor) {
-        res = 0;
-        visited.clear();
-        genPal(new int[total], 0, total - 1, divisor, total);
-        return res;
+        return ans;
     }
 }
